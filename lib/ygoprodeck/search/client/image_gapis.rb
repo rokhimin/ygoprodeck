1ec506d1f86e8c34bd1197550b4b46f371970d88
rokhimin <rokhim.whd@gmail.com>
2019-10-28 13:21:24 +0000

    IMAGE SMALL CARD ADDED

--- lib/ygoprodeck/search/client/image_gapis.rb
+++ lib/ygoprodeck/search/client/image_gapis.rb
@@ -6,4 +6,11 @@ module Ygoprodeck
 			"https://storage.googleapis.com/ygoprodeck.com/pics/#{image_by_id}.jpg"
 		end
 	end
+	
+	class Image_small
+		attr_reader :image_small_by_id
+		def self.is(image_by_id)
+			"https://storage.googleapis.com/ygoprodeck.com/pics_small/#{image_small_by_id}.jpg"
+		end
+	end
 end
