6deb6ddd2e5ca775452eabe3be479394eb7090a7
rokhimin <rokhim.whd@gmail.com>
2019-10-28 14:22:18 +0000

    EXAMPLE ADDED

---
+++ examples/search.rb
@@ -0,0 +1,21 @@
+require 'ygoprodeck'
+
+# yugi is example, you free to change variable name
+yugi = Ygoprodeck::Fname.is('dark magician') # example I wanna search 'dark magician' using fuzzy search
+
+puts yugi # => get all information of dark magician (name,id,level,attr,atk,def,etc)
+
+puts '#' *10
+
+# => get specific information of dark magician
+puts yugi['id']
+puts yugi['name'] 
+puts yugi['attribute'] 
+puts yugi['type'] 
+puts yugi['race'] 
+puts yugi['level'] 
+puts yugi['desc']
+puts yugi['atk'] 
+puts yugi['def']
+puts Ygoprodeck::Image.is(yugi['id']) # => use id from yugi['id'] to get dark magician image
+puts Ygoprodeck::Image_small.is(yugi['id']) # => small version
--- examples/search_card.rb
+++
@@ -1,8 +0,0 @@
-require 'ygoprodeck'
-
-z = Ygoprodeck::List.is('dark')
-puts z
-puts '#' *10
-#puts Ygoprodeck::Image.is(z['id'])
-puts '#' *10
-#puts Ygoprodeck::Image_small.is(z['id'])
