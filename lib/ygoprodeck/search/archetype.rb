b69537995560bd930d5d588a13c9337d7ee22055
rokhimin <rokhim.whd@gmail.com>
2019-10-28 14:01:25 +0000

    ARCHETYPE SEARCH ADDED

---
+++ lib/ygoprodeck/search/archetype.rb
@@ -0,0 +1,21 @@
+
+module Ygoprodeck
+	class Archetype
+		attr_reader :archetype
+		def self.is(archetype)
+			url = "#{Ygoprodeck::Endpoint.is}?archetype=#{archetype}"
+			uri = URI(url)
+			response = Net::HTTP.get(uri)
+			load = JSON.parse(response)
+			
+			if load[0] == nil
+				'{"error"=>"No card matching your query was found in the database."}'
+			else
+				load
+			end
+			
+		end
+	end
+	
+end
+
