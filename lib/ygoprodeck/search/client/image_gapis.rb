65514f5cbe5dd16a5d56766b5b95f81be9756a32
rokhimin <rokhim.whd@gmail.com>
2024-12-30 10:11:12 +0700

    BUG IMAGE APIS YGOPRODECK

--- lib/ygoprodeck/search/client/image_gapis.rb
+++ lib/ygoprodeck/search/client/image_gapis.rb
@@ -3,14 +3,14 @@ module Ygoprodeck
 	class Image
 		attr_reader :image_by_id
 		def self.is(image_by_id)
-			"https://storage.googleapis.com/ygoprodeck.com/pics/#{image_by_id}.jpg"
+			"https://images.ygoprodeck.com/images/cards/#{image_by_id}.jpg"
 		end
 	end
 	
 	class Image_small
 		attr_reader :image_small_by_id
 		def self.is(image_small_by_id)
-			"https://storage.googleapis.com/ygoprodeck.com/pics_small/#{image_small_by_id}.jpg"
+			"https://images.ygoprodeck.com/images/cards_cropped/#{image_small_by_id}.jpg"
 		end
 	end
 end
